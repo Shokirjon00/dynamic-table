2cdc8bf4c8c9f9ee1b9a50c235bced912cf124ef
Sh_Jabborov <jabborovshokirjon69@gmail.com>
2024-09-03 10:21:00 +0500

    feat: optimization scripts

--- src/app/dynamic-table/dynamic-table.component.ts
+++ src/app/dynamic-table/dynamic-table.component.ts
@@ -1,13 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TableComponent } from "./table/table.component";
 import { DataService } from "./service/data.service";
 import { DataRow } from "./interface/data";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { finalize } from "rxjs";
+import { delay, finalize } from "rxjs";
 import { CommonModule } from "@angular/common";
 import { LoaderComponent } from "../shared/loader/loader.component";
 import { FilterComponent } from "../shared/filter/filter.component";
-
 
 @Component({
   selector: 'app-dynamic-table',
@@ -22,21 +21,24 @@ import { FilterComponent } from "../shared/filter/filter.component";
   templateUrl: './dynamic-table.component.html',
   styleUrl: './dynamic-table.component.scss'
 })
-export class DynamicTableComponent {
-  datatable: DataRow[] = [];
-  isLoading: boolean = true;
+export class DynamicTableComponent implements OnInit {
+  datatable: DataRow[];
+  isLoading: boolean;
 
-  constructor(private dataService: DataService) {
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
     this.getData();
   }
 
   private getData(): void {
-    this.isLoading = true
+    this.isLoading = true;
     this.dataService.getData()
       .pipe(
-        finalize(() => this.isLoading = false),
-        takeUntilDestroyed()
+        finalize(() => this.isLoading = false)
       )
-      .subscribe((res: DataRow[] | any) => this.datatable = res);
+      .subscribe((res: DataRow[] | any) => {
+        this.datatable = res;
+      });
   }
 }
